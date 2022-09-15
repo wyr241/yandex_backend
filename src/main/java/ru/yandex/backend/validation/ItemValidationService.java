e56f3f6b22ad217d5b131e1ddac875dc187ce89a
wyr241 <yerzhan.rzagaliyev@gmail.com>
2022-09-16 01:19:51 +0500

    Added validation logic

--- src/main/java/ru/yandex/backend/validation/ItemValidationService.java
+++ src/main/java/ru/yandex/backend/validation/ItemValidationService.java
@@ -4,13 +4,19 @@ import org.springframework.stereotype.Component;
 import ru.yandex.backend.domain.NodeType;
 import ru.yandex.backend.dto.Item;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Component
 public class ItemValidationService {
 
     public boolean isValid(List<Item> items) {
+        Set<String> ids = new HashSet<String>();
         for (Item item: items) {
+            if(ids.contains(item.getId())) return false;
+            else ids.add(item.getId());
+
             if (!isValid(item)) return false;
         }
         return true;
@@ -18,7 +24,7 @@ public class ItemValidationService {
 
     private boolean isValid(Item item) {
         // implement here
-
+        if(item.getId() == null) return false;
         if(item.getType() == NodeType.FOLDER){
             if(item.getUrl() != null) return false;
             if(item.getSize() != null) return false;
