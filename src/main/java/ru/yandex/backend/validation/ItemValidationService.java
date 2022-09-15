84f989164a55918da63da10a35eacacdf7173d0c
wyr241 <yerzhan.rzagaliyev@gmail.com>
2022-09-16 01:12:18 +0500

    Added validation logic

--- src/main/java/ru/yandex/backend/validation/ItemValidationService.java
+++ src/main/java/ru/yandex/backend/validation/ItemValidationService.java
@@ -1,6 +1,7 @@
 package ru.yandex.backend.validation;
 
 import org.springframework.stereotype.Component;
+import ru.yandex.backend.domain.NodeType;
 import ru.yandex.backend.dto.Item;
 
 import java.util.List;
@@ -12,13 +13,19 @@ public class ItemValidationService {
         for (Item item: items) {
             if (!isValid(item)) return false;
         }
-
         return true;
     }
 
     private boolean isValid(Item item) {
         // implement here
 
-        return false;
+        if(item.getType() == NodeType.FOLDER){
+            if(item.getUrl() != null) return false;
+            if(item.getSize() != null) return false;
+        } else if (item.getType() == NodeType.FILE) {
+            if(item.getUrl().length() <= 255) return false;
+            if(item.getSize() <= 0) return false;
+        }
+        return true;
     }
 }
